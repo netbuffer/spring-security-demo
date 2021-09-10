ce52683ea856ffa610f45c0e19e969a6ad0c86e4
netbuffer <javawiki@163.com>
2021-09-10 22:11:07 +0800

    use UserDetailsService to login

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # spring-security-demo
-> spring-security test project
+> spring-security test project  
+* https://github.com/netbuffer/spring-security-demo
+* https://gitee.com/netbuffer/spring-security-demo
 
 ### help
 * look for the default password generated from the console:`Using generated security password:` default generated user is `user` 
---
+++ src/main/java/cn/netbuffer/springsecuritydemo/service/UserService.java
@@ -0,0 +1,35 @@
+package cn.netbuffer.springsecuritydemo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class UserService implements UserDetailsService {
+
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
+    //login process
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //todo find user from database/redis...
+        if (!username.equals("admin")) {
+            return null;
+        }
+        log.debug("loadUserByUsername invoked username={}", username);
+        String password = passwordEncoder.encode("admin");
+        List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+        return new User(username, password, grantedAuthorityList);
+    }
+
+}
