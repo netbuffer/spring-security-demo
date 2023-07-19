3e54b5489e17ad35e8e7da28ff1196d492a8abad
netbuffer <javawiki@163.com>
2023-07-19 09:41:48 +0800

    test @PostAuthorize

--- src/main/java/cn/netbuffer/springsecuritydemo/controller/AppController.java
+++ src/main/java/cn/netbuffer/springsecuritydemo/controller/AppController.java
@@ -1,7 +1,10 @@
 package cn.netbuffer.springsecuritydemo.controller;
 
+import cn.netbuffer.springsecuritydemo.pojo.DataObject;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import java.util.Map;
@@ -20,6 +23,15 @@ public class AppController {
     @GetMapping("access")
     public String access() {
         return "access";
+    }
+
+    @PostAuthorize("returnObject.owner == principal.username")
+    @GetMapping("resource")
+    public DataObject resource(String owner) {
+        log.debug("resource get principal={}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        DataObject dataObject = new DataObject();
+        dataObject.setOwner(owner);
+        return dataObject;
     }
 
     @PostMapping("csrf-test")
---
+++ src/main/java/cn/netbuffer/springsecuritydemo/pojo/DataObject.java
@@ -0,0 +1,11 @@
+package cn.netbuffer.springsecuritydemo.pojo;
+
+import lombok.Data;
+
+@Data
+public class DataObject {
+
+    private String owner;
+    private String data;
+
+}
